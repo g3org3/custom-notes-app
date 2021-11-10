a1bdc7a3fb143b63ebce3651d444f1246dbb673b
Jorge Adolfo <7jagjag@gmail.com>
2021-11-10 13:46:57 +0100

    Add types to container div

--- src/pages/Root/Root.style.ts
+++ src/pages/Root/Root.style.ts
@@ -1,10 +1,14 @@
 // @ts-ignore
 import styled from 'styled-components'
 
+interface DivProps {
+  isDarkTheme?: boolean
+}
+
 export const Container = styled.div`
   position: fixed;
   top: 48px;
-  background: ${(props) => (props.isDarkTheme ? '#333' : '#eee')};
+  background: ${(props: DivProps) => (props.isDarkTheme ? '#333' : '#eee')};
   height: calc(100vh - 48px);
   padding: 20px 0 50px 0;
   width: 100vw;
