7787916e1d96493b730329aa435da367cbe3602a
Jorge Adolfo <7jagjag@gmail.com>
2021-11-12 01:55:37 +0100

    export done

--- src/pages/Export/Export.component.tsx
+++ src/pages/Export/Export.component.tsx
@@ -1,12 +1,16 @@
 import Paper from '@mui/material/Paper'
+import { createRef } from 'react'
 import { useSelector } from 'react-redux'
 import yaml from 'js-yaml'
 
 import { selectNotes } from 'modules/Note/Note.selectors'
+import Pre from 'components/Pre'
 
 interface Props {
   path?: string
 }
+
+const ref = createRef<HTMLTextAreaElement>()
 
 const Export = (props: Props) => {
   const notes = useSelector(selectNotes)
@@ -15,9 +19,21 @@ const Export = (props: Props) => {
     noArrayIndent: true,
   })
 
+  setTimeout(() => {
+    if (ref.current) {
+      ref.current.select()
+      document.execCommand('copy')
+    }
+  }, 1000)
+
   return (
-    <Paper>
-      <pre>{str}</pre>
+    <Paper sx={{ padding: '20px' }}>
+      <Pre>{str}</Pre>
+      <textarea
+        style={{ position: 'fixed', top: -1000 }}
+        ref={ref}
+        value={str}
+      />
     </Paper>
   )
 }
