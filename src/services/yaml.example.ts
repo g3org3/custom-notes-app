be654e807b9e4b6b64d5fe6408c9c0f08cad23e8
george <7jagjag@gmail.com>
2022-01-17 02:01:58 +0100

    update example

--- src/services/yaml.example.ts
+++ src/services/yaml.example.ts
@@ -1,42 +1,43 @@
-export const exampleNotesYaml = `- subject: Big meeting
-  date: 2021-10-14T18:00:00+01:00
-  people: [Bob, Mike]
-  notes: we talk about bla bla
-  tags: [tech, note-taking]
-  doubts:
-  - how to write multiline notes?
-  ns:
-  - (done) finish my homework
-  - (done:2022-01-02T12:00:00+01:00) finish the new song
+export const exampleNotesYaml = `--- 
+subject: Big meeting
+date: 2021-10-14T18:00:00+01:00
+people: [Bob, Mike]
+notes: we talk about bla bla
+tags: [tech, note-taking]
+doubts:
+- how to write multiline notes?
+ns:
+- (done) finish my homework
+- (done:2022-01-02T12:00:00+01:00) finish the new song
+---
+subject: dashboard design
+date: 2021-10-15
+people:
+- bob
+- rachel
+notes: |
+  with the pipe then you are able to 
+  write multiline notes
 
-- subject: dashboard design
-  date: 2021-10-15
-  people:
-  - bob
-  - rachel
-  notes: |
-    with the pipe then you are able to 
-    write multiline notes
+  and everything will be considered as a string
 
-    and everything will be considered as a string
-
-    ### markdown syntax
-    
-    | name        | version |
-    |-------------|---------|
-    | note-taking | 1.2.3   |
-
-  next_steps:
-  - (done:2022-01-02T16:00:00) create the wire-frame
-  - (done) dj mix
-
-- subject: merge?
-  next_steps:
-  - create the wire-frame (owner robert)
+  ### markdown syntax
   
-- subject: another one
-  date: 2021-11-01
-  tags: [notes, tech]
-  next_steps:
-  - create UX links (owner robert)
+  | name        | version |
+  |-------------|---------|
+  | note-taking | 1.2.3   |
+
+next_steps:
+- (done:2022-01-02T16:00:00) create the wire-frame
+- (done) dj mix
+---
+subject: merge?
+next_steps:
+- create the wire-frame (owner robert)  
+---
+subject: another one
+date: 2021-11-01
+tags: [notes, tech]
+next_steps:
+- create UX links (owner robert)
 `
